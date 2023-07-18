c2aa2781c8f5d30d845734763e2ea9ad6fff2c66
Bradley Chery <Bradley434@hotmail.com>
2023-07-18 00:59:09 -0400

    added more files to the header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from 'react'
 import logo from "../../assets/images/logo.png";
+import userImg from '../../assets/images/avatar-icon.png'
 import { NavLink, Link } from 'react-router-dom';
+import { BiMenu } from 'react-icons/bi'
 
 const navLinks = [
     {
@@ -33,7 +35,7 @@ const Header = () => {
 
                 {/* ===================== menu ==================== */}
                 <div className="navigation">
-                    <ul className="menu flex items-center gap-[2.7rem">
+                    <ul className="menu flex items-center gap-[2.7rem]">
                         {
                             navLinks.map((link, index) => (
                                 <li key={index}>
@@ -42,7 +44,7 @@ const Header = () => {
                                         className={navClass =>
                                             navClass.isActive
                                                 ? "text-primaryColor text-[16px] leading-7 font-[600]"
-                                                : "text-textColor text-[16px] leading-7 font-[500]"
+                                                : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
                                         }
                                     >
                                         {link.display}
@@ -50,6 +52,25 @@ const Header = () => {
                                 </li>
                             ))}
                     </ul>
+                </div>
+
+                {/* =================== nav right ======================  */}
+                <div className="flex items-center gap-4">
+                    <div>
+                        <Link to='/'>
+                            <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
+                                <img src={userImg} className="w-full rounded-full" alt="" />
+                            </figure>
+                        </Link>
+                    </div>
+
+                    <Link to="/login">
+                        <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>Login</button>
+                    </Link>
+
+                    <span className='md:hidden'>
+                        <BiMenu className="w-6 h-6 cursor-pointer" />
+                    </span>
                 </div>
             </div>
         </div>
