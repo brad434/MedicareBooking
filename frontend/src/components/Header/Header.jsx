c5697b6e9aeb066a0ce48b4833b9608e5bdcc5e7
Bradley Chery <Bradley434@hotmail.com>
2023-07-18 21:36:58 -0400

    added to the app.css creating side menu bar and media query

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -24,7 +24,29 @@ const navLinks = [
 ]
 
 const Header = () => {
-    return <header className='header flex items-center'>
+
+    const headerRef = useRef(null);
+    const menuRef = useRef(null);
+
+    const handleStickyHeader = () => {
+        window.addEventListener('scroll', () => {
+            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+                headerRef.current.classList.add('sticky__header')
+            } else {
+                headerRef.current.classList.remove('sticky__header')
+            }
+        })
+    }
+
+    useEffect(() => {
+        handleStickyHeader()
+
+        return () => window.removeEventListener('scroll', handleStickyHeader)
+    })
+
+    const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
+    return <header className='header flex items-center' ref={headerRef}>
         <div className="container">
             <div className="flex items-center justify-between">
                 {/* ============== logo =============== */}
@@ -34,7 +56,7 @@ const Header = () => {
 
 
                 {/* ===================== menu ==================== */}
-                <div className="navigation">
+                <div className="navigation" ref={menuRef} onClick={toggleMenu}>
                     <ul className="menu flex items-center gap-[2.7rem]">
                         {
                             navLinks.map((link, index) => (
@@ -56,7 +78,7 @@ const Header = () => {
 
                 {/* =================== nav right ======================  */}
                 <div className="flex items-center gap-4">
-                    <div>
+                    <div className='hidden'>
                         <Link to='/'>
                             <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
                                 <img src={userImg} className="w-full rounded-full" alt="" />
@@ -68,7 +90,7 @@ const Header = () => {
                         <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>Login</button>
                     </Link>
 
-                    <span className='md:hidden'>
+                    <span className='md:hidden' onClick={toggleMenu}>
                         <BiMenu className="w-6 h-6 cursor-pointer" />
                     </span>
                 </div>
