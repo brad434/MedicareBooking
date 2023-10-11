1257ade9f29d054df670effead72fb82f9540edc
Bradley Chery <Bradley434@hotmail.com>
2023-10-11 02:31:31 -0400

    updated files

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import icon01 from '../assets/images/icon01.png';
 import icon02 from '../assets/images/icon02.png';
 import icon03 from '../assets/images/icon03.png';
 import featureImg from '../assets/images/feature-img.png';
+import faqImg from '../assets/images/faq-img.png';
 import videoIcon from '../assets/images/video-icon.png';
 import avatarIcon from '../assets/images/avatar-icon.png';
 import { Link } from 'react-router-dom';
@@ -14,6 +15,8 @@ import { BsArrowRight } from 'react-icons/bs';
 import About from '../components/About/About';
 import ServiceList from '../components/Services/ServiceList';
 import DoctorList from '../components/Doctors/DoctorList';
+import FaqList from '../components/Faq/FaqList';
+import Testimonial from '../components/Testimonial/Testimonial';
 
 
 const Home = () => {
@@ -28,7 +31,7 @@ const Home = () => {
                         <div>
                             <div className='lg:w-[570px]'>
                                 <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]'>
-                                    We help patients live a healthy, longer life.
+                                    We help patients receive healthier and enjoyable life.
                                 </h1>
 
                                 <p className='text__para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo cupiditate repudiandae magnam dicta alias, perspiciatis placeat et, minima libero modi ipsum. Blanditiis magnam quos animi.</p>
@@ -224,10 +227,44 @@ const Home = () => {
                     <DoctorList />
                 </div>
             </section>
-
-
-
             {/* =========== Doctors End=========== */}
+
+            {/* ============= FAQ Start ============ */}
+            <section>
+                <div className="container">
+                    <div className="flex justify-between gap-[50px] lg:gap-0">
+                        <div className="w-1/2 hidden md:block">
+                            <img src={faqImg} alt="" />
+                        </div>
+
+                        <div className='w-full md:w-1/2'>
+                            <h2 className='heading'>
+                                Most questions by our beloved patients
+                            </h2>
+
+                            <FaqList />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* ============= FAQ END ============ */}
+
+            {/* ============== testimonial ============== */}
+            <section>
+                <div className="container">
+                    <div className="xl:w-[470px] mx-auto">
+                        <h2 className="heading text-center">What our patients say</h2>
+                        <p className='text__para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
+                    </div>
+
+                    <Testimonial />
+                </div>
+            </section>
+
+
+
+            {/* ============== testimonial END ============== */}
+
         </>
     )
 }
