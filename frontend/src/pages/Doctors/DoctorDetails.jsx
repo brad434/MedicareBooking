f0af4847feea7a71476138b55b26ee0290db7766
Bradley Chery <Bradley434@hotmail.com>
2023-10-12 02:25:44 -0400

    added an About and Feedback section to find a doctor

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import doctorImg from '../../assets/images/doctor-img02.png'
 import starIcon from '../../assets/images/Star.png'
+import DoctorAbout from './DoctorAbout';
+import Feedback from './Feedback';
 
 const DoctorDetails = () => {
+    const [tab, setTab] = useState('about');
+
+
     return <section>
         <div className="max-w-[1170px] px-5 mx-auto">
             <div className="grid md:grid-cols-3 gap-[50px]">
@@ -27,10 +32,28 @@ const DoctorDetails = () => {
                                 <span className='flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor'>
                                     <img src={starIcon} alt="" /> 4.8
                                 </span>
+                                <span className='text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400px] text-textColor'>
+                                    (272)
+                                </span>
                             </div>
+
+                            <p className='text__para text-[14px] leading-5 md:text-[15px] lg:max-w-[390px]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis voluptatum temporibus, ipsam culpa libero porro!!</p>
                         </div>
                     </div>
+
+                    {/* ----------------- Created the 'About' & 'Feedback' section ------------ */}
+                    <div className="mt-[50px] border-b border-solid border-[#0066ff34]">
+                        <button onClick={() => setTab('about')} className={`${tab === 'about' && 'border-b border-solid border-primaryColor'} py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}>About</button>
+                        <button onClick={() => setTab('feedback')} className={`${tab === 'feedback' && 'border-b border-solid border-primaryColor'} py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}>Feedback</button>
+                    </div>
+
+                    <div className="mt-[50px]">
+                        {tab === 'about' && <DoctorAbout />}
+                        {tab === 'feedback' && <Feedback />}
+                    </div>
+
                 </div>
+                <div></div>
             </div>
         </div>
     </section>
