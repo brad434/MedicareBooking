7b6037b8d34627453f5b9048eafe11957d857d66
Bradley Chery <Bradley434@hotmail.com>
2023-10-15 14:02:47 -0400

    added and completed sidePanel

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -5,10 +5,15 @@ const FeedbackForm = () => {
 
     const [rating, setRating] = useState(0)
     const [hover, setHover] = useState(0)
+    const [reviewText, setReviewText] = useState("")
+
+    const handleSubmitReview = async e => {
+        e.preventDefault();
+    }
 
     return <form action=''>
         <div>
-            <h3 className='text-headingColor text-[16px] leading-6 font-semibold mb-4'>How would you rate the overall experience?*</h3>
+            <h3 className='text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0'>How would you rate the overall experience?*</h3>
 
             {/* ---- This div allows us to choose any amount of rating stars for the feedback of doctor section ------ */}
             <div>
@@ -37,7 +42,16 @@ const FeedbackForm = () => {
                 })}
             </div>
         </div>
-    </form >
+
+
+        <div className='mt-[30px]'>
+            <h3 className='text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0'>Share your feedback or suggestions*</h3>
+
+            <textarea className='border border-solid border-[#0066ff34] focus:outline outline-primaryColor w-full px-4 py-3 rounded-md' row="5" placeholder='Write your message' onChange={() => setReviewText(e.target.value)}> Write your </textarea>
+        </div>
+
+        <button type='submit' onClick={handleSubmitReview} className='btn'>Submit Feedback</button>
+    </form>
 }
 
 export default FeedbackForm;
