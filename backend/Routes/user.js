0f3414f161dadae824708fee6f07f12bf62390ba
Bradley Chery <Bradley434@hotmail.com>
2023-10-21 01:52:22 -0400

    updated user.js and created verifytoken.js

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -6,12 +6,12 @@ import {
   getSingleUser,
 } from "../Controllers/userController.js";
 
-import { authenticate } from "../auth/verifyToken.js";
+import { authenticate, restrict } from "../auth/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getAllUser);
-router.get("/:id", authenticate, getSingleUser);
+router.get("/:id", authenticate, restrict(["patient"]), getSingleUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
--- backend/auth/verifyToken.js
+++ backend/auth/verifyToken.js
@@ -32,3 +32,29 @@ export const authenticate = async (req, res, next) => {
     return res.status(401).json({ success: false, message: "Invalid token" });
   }
 };
+
+export const restrict = roles => async (req, res, next) => {
+  const userId = req.userId;
+
+  let user;
+
+  const patient = await User.findById(userId);
+  const doctor = await Doctor.findById(userId);
+
+  if (patient) {
+    user = patient;
+  } else if (doctor) {
+    user = doctor;
+  } else {
+    return res.status(401).json({ success: false, message: "User not found." });
+  }
+
+  if (!roles.includes(user.role)) {
+    return res.status(401).json({
+      success: false,
+      message: "You are not authorized for this action.",
+    });
+  }
+
+  next();
+};
