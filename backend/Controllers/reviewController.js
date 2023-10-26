fe03729b1b622aea03330f1c7d9d6f0933976fa7
Bradley Chery <Bradley434@hotmail.com>
2023-10-26 02:05:12 -0400

    updated reviewController and finish 90% of controller for review.js

--- backend/Controllers/reviewController.js
+++ backend/Controllers/reviewController.js
@@ -1,2 +1,38 @@
 import Review from "../models/ReviewSchema.js";
 import Doctor from "../models/DoctorSchema.js";
+
+//get all reviews
+export const getAllReviews = async (req, res) => {
+  const reviews = await Review.find({});
+
+  try {
+    const reviews = await Reviews.Find({});
+
+    res.status(200).json({ success: true, message: "Not found" });
+  } catch (err) {
+    res.status(400).json({ success: false, message: "Not found" });
+  }
+};
+
+//create review
+
+export const createReview = async (req, res) => {
+  if (!req.body.doctor) req.body.doctor = req.body.doctorId;
+  if (!req.body.user) req.body.user = req.body.userId;
+
+  const newReview = new Review(req.body);
+
+  try {
+    const savedReview = await newReview.save();
+
+    await Doctor.findByIdAndUpdate(req.body.doctor, {
+      $push: { reviews: savedReview._id },
+    });
+
+    res
+      .status(200)
+      .json({ success: true, message: "Review submitted", data: savedReview });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
